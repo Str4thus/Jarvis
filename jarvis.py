89f5714a11e072bf3eceedae1ab57489fc033613
Str4thus <str4thus@protonmail.com>
2021-03-16 15:42:08 +0100

    fixed dynamic modules

--- jarvis.py
+++ jarvis.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 
 import os
+import sys
 import argparse
 import importlib
-from inspect import signature 
+from pathlib import Path
+
+
+def get_script_path():
+    return Path(os.path.dirname(os.path.realpath(sys.argv[0])))
 
 _MODULE_DICT = {}
 parser = argparse.ArgumentParser()
 sub_parsers = parser.add_subparsers(dest="subcommand")
 
 # import modules and link parsers
-for module_file in os.listdir("modules"):
+modules_path = get_script_path() / "modules"
+print(modules_path)
+for module_file in os.listdir(modules_path):
 	if ".py" not in module_file:
 		continue
 	module_name = module_file.split(".")[0]
-	module = importlib.import_module("modules." + module_name)
+	module = importlib.import_module(f"modules.{module_name}", package=modules_path)
 	module.add_parser(sub_parsers)
 	_MODULE_DICT[module_name] = module.main
 
